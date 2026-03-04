9bb41f1d45458f7e662661ed1f3afe08d3af93c2
Ritwik Upadhyay <134372879+ritwik-upadhyay@users.noreply.github.com>
2026-03-04 22:38:52 +0530

    venv addition

--- app.py
+++ app.py
@@ -172,5 +172,6 @@ def result():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5050))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", "5050"))
+    print(f"Open http://127.0.0.1:{port}")
+    app.run(debug=True, host="127.0.0.1", port=port, use_reloader=False)
